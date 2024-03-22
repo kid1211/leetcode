e36017454a0d217d96db42267210826abf33ca93
Xujie Zheng <z374201743@gmail.com>
2024-03-22 14:34:54 -0400

    Time: 64 ms (93.28%) | Memory: 19.5 MB (21.25%) - LeetSync

---
+++ 279-perfect-squares/perfect-squares.py
@@ -0,0 +1,47 @@
+class Solution:
+    def numSquares(self, n: int) -> int:
+        sqrt_nums = set([ i ** 2 for i in range(1, int(n**0.5) + 1)])
+
+        @cache
+        def dfs(remain, ans):
+            if ans == 1:
+                return remain in sqrt_nums
+            
+            for num in sqrt_nums:
+                # if remain < num:
+                #     break
+    
+                if dfs(remain - num, ans - 1):
+                    return True
+            return False
+        
+        for ans in range(1, n + 1):
+            if dfs(n, ans):
+                return ans
+
+        return False
+
+# class Solution:
+#     def numSquares(self, n: int) -> int:
+#         sqrt_nums = [ i ** 2 for i in range(1, int(n**0.5) + 1)]
+        
+#         @cache
+#         def dfs(startIdx, remain):
+#             # if startIdx >= len(sqrt_nums):
+#             #     return sys.maxsize
+            
+#             res = sys.maxsize
+#             for i in range(startIdx, len(sqrt_nums)):
+#                 val = sqrt_nums[i]
+
+#                 if remain < val:
+#                     break
+                
+#                 if remain == val:
+#                     res = 1
+#                     break
+                
+#                 res = min(res, 1 + dfs(i, remain - val))
+#             return res
+        
+#         return dfs(0, n)
