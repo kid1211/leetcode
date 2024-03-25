40eb47aaa1aa9c6aa9cd23bc54af6a782a5e1b9c
Xujie Zheng <z374201743@gmail.com>
2024-03-25 16:07:27 -0400

    Time: 986 ms (32.47%) | Memory: 20.9 MB (20.27%) - LeetSync

---
+++ 15-3sum/3sum.py
@@ -0,0 +1,34 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        
+        def twoSumSorted(left, right, target) -> [[int, int, int]]:
+            res = []
+            while left < right:
+                if res and nums[left] == nums[left - 1]:
+                    print('huh')
+                    left += 1
+                    continue
+                if res and nums[right] == nums[right + 1]:
+                    print('what')
+                    right -= 1
+                    continue
+                tmp = nums[left] + nums[right] + target
+
+                if tmp == 0:
+                    res += [[nums[left], nums[right], target]]
+                    left += 1
+                    right -= 1
+                elif tmp < 0:
+                    left += 1
+                else:
+                    right -= 1
+            return res
+        
+        nums.sort()
+        n = len(nums)
+        res = []
+        for i in range(len(nums)):
+            if res and nums[i] == nums[i - 1]:
+                continue
+            res += twoSumSorted(i + 1, n - 1, nums[i])
+        return res
