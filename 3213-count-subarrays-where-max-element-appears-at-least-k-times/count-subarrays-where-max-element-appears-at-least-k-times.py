27933a3612717143927ab091feb699d5be915753
Xujie Zheng <z374201743@gmail.com>
2024-03-29 18:39:25 -0400

    Time: 1002 ms (18.65%) | Memory: 32.6 MB (21.03%) - LeetSync

--- 3213-count-subarrays-where-max-element-appears-at-least-k-times/count-subarrays-where-max-element-appears-at-least-k-times.py
+++ 3213-count-subarrays-where-max-element-appears-at-least-k-times/count-subarrays-where-max-element-appears-at-least-k-times.py
@@ -1,14 +1,32 @@
 class Solution:
     def countSubarrays(self, nums: List[int], k: int) -> int:
-        max_element = max(nums)
-        ans = start = max_elements_in_window = 0
+        maxElement = max(nums)
+        counter = defaultdict(int)
 
-        for end in range(len(nums)):
-            if nums[end] == max_element:
-                max_elements_in_window += 1
-            while max_elements_in_window == k:
-                if nums[start] == max_element:
-                    max_elements_in_window -= 1
-                start += 1
-            ans += start
-        return ans
+        left = res = 0
+        for right in range(len(nums)):
+            counter[nums[right]] += 1
+            while left <= right and counter[maxElement] == k:
+                counter[nums[left]] -= 1
+                left += 1
+            # if counter[maxElement] >= k:
+            #     print(left, right)
+            res += left
+                
+        # 1, 3
+        return res
+
+# class Solution:
+#     def countSubarrays(self, nums: List[int], k: int) -> int:
+#         max_element = max(nums)
+#         ans = start = max_elements_in_window = 0
+
+#         for end in range(len(nums)):
+#             if nums[end] == max_element:
+#                 max_elements_in_window += 1
+#             while max_elements_in_window == k:
+#                 if nums[start] == max_element:
+#                     max_elements_in_window -= 1
+#                 start += 1
+#             ans += start
+#         return ans
