8dad8d6f9e4d09b9f0a38da4de135cd74d0a6f81
Xujie Zheng <z374201743@gmail.com>
2024-03-30 17:00:44 -0400

    Time: 346 ms (53.20%) | Memory: 19.9 MB (59.58%) - LeetSync

--- 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.py
+++ 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.py
@@ -1,27 +1,18 @@
 class Solution:
     def subarraysWithKDistinct(self, nums: List[int], k: int) -> int:
-        return self.slidingWindowAtMost(nums, k) - self.slidingWindowAtMost(nums, k - 1)
+        def slidingWindowAtMostK(k):
+            if k == 0:
+                return 0
+            left = res = 0
+            unique = defaultdict(int)
+            for right in range(len(nums)):
+                unique[nums[right]] += 1
 
-    # Helper function to count the number of subarrays with at most k distinct elements.
-    def slidingWindowAtMost(self, nums: List[int], distinctK: int) -> int:
-        # To store the occurrences of each element.
-        freq_map = defaultdict(int)
-        left = 0
-        total_count  = 0
-
-        # Right pointer of the sliding window iterates through the array.
-        for right in range(len(nums)):
-            freq_map[nums[right]] += 1
-
-            # If the number of distinct elements in the window exceeds k,
-            # we shrink the window from the left until we have at most k distinct elements.
-            while len(freq_map) > distinctK:
-                freq_map[nums[left]] -= 1
-                if freq_map[nums[left]] == 0:
-                    del freq_map[nums[left]]
-                left += 1
-
-            # Update the total count by adding the length of the current subarray.
-            total_count  += right - left + 1
-
-        return total_count 
+                while left < right and len(unique) > k:
+                    unique[nums[left]] -= 1
+                    if unique[nums[left]] == 0:
+                        del unique[nums[left]]
+                    left += 1
+                res += right - left + 1
+            return res
+        return slidingWindowAtMostK(k) - slidingWindowAtMostK(k - 1)
