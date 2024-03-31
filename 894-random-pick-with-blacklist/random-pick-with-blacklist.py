ef08a213bf1f54c607dda195c2fe4a5fc6ff37db
Xujie Zheng <z374201743@gmail.com>
2024-03-31 12:08:18 -0400

    Time: 288 ms (18.55%) | Memory: 26.3 MB (84.68%) - LeetSync

--- 894-random-pick-with-blacklist/random-pick-with-blacklist.py
+++ 894-random-pick-with-blacklist/random-pick-with-blacklist.py
@@ -1,50 +1,32 @@
 class Solution:
 
     def __init__(self, n: int, blacklist: List[int]):
-        self.blacklist = sorted(blacklist)
-        self.size = n
+        self.b = sorted(blacklist)
+        self.n = n
 
     def pick(self) -> int:
-        k = random.randint(0, self.size - len(self.blacklist) - 1)
-        left, right = 0, len(self.blacklist) - 1
+        k = random.randint(0, self.n - len(self.b) - 1) # exclusive
+        left, right = 0, len(self.b) - 1
 
         while left + 1 < right:
-            mid = (left + right) // 2 # +1?
-            #find the largest blacklist[mid] less than the W[k]
-            if self.blacklist[mid] <= k + mid:
+            mid = (left + right) // 2
+
+            if self.b[mid] - mid <= k:
                 left = mid
             else:
                 right = mid
         
-        # print(left, right, len(self.blacklist))
-        # print('2', k, k + left, + 1, k + right + 1)
-
-        if left >= len(self.blacklist):
+        if left >= len(self.b):
+            return k
+        
+        if self.b[right] - right <= k:
+            return right + k + 1
+        if self.b[left] - left <= k:
+            return left + k + 1
+        else:
             return k
 
-        if self.blacklist[right] <= k + right:
-            return k + right + 1
-        elif self.blacklist[left] <= k + left:
-            return k + left + 1
-        return k
 
-# 0   1. 2. 3. 4. 5.  6. 7.    8
-# [2, 4, 6, 7, 9, 10, 11, 13, 17]
-# [2, 3, 4, 4, 5,  5,  5,  6,  9] 
-
-# k == 0, 0
-# k == 1, 1
-# k == 2, 2 + 1 + 0. <= 2
-# k == 3, 3 + 1 + 1 == 5
-# k == 4, 4 + 1 + 3 == 8
-# k == 5, 5 + 1 + 6 == 12
-# k == 6, 6 + 1 + 7 == 14
-# k == 7, 7 + 1 + 7 == 15
-# k == 8, 8 + 1 + 7 == 16
-# k == 9, 9 + 1 + 8 == 18
-# return lo == hi && b[lo] - lo <= k ? k + lo + 1 : k;
-        
-# total - blacklist = whitelist range
 
 
 # Your Solution object will be instantiated and called as such:
