f7db7ef4ca64dba8f742a8ba7d9fbd56476a75f2
Xujie Zheng <z374201743@gmail.com>
2024-04-01 14:49:30 -0400

    Time: 151 ms (26.62%) | Memory: 23.3 MB (8.82%) - LeetSync

--- 218-the-skyline-problem/the-skyline-problem.py
+++ 218-the-skyline-problem/the-skyline-problem.py
@@ -1,8 +1,7 @@
 from sortedcontainers import SortedList
 class Solution:
     def getSkyline(self, buildings: List[List[int]]) -> List[List[int]]:
-        heightStack = SortedList()
-        endStack = SortedList()
+        stack = SortedList()
         res = []
 
         tmp = []
@@ -13,21 +12,17 @@ class Solution:
 
         for pos, currHeight_end, height, isEnd in sorted(tmp):
             if isEnd:
-                # removeList = []
-                # for heightStack_height, height_end in heightStack:
-                #     if pos >= height_end:
-                #         removeList += [(heightStack_height, height_end)]
-                # for item in removeList:
-                #     heightStack.remove(item)
-                while endStack and pos >= -endStack[-1][0]:
-                    neg_end, pos_height = endStack.pop()
-                    heightStack.remove((pos_height, - neg_end))
+                removeList = []
+                for stack_height, height_end in stack:
+                    if pos >= height_end:
+                        removeList += [(stack_height, height_end)]
+                for item in removeList:
+                    stack.remove(item)
             else:
-                heightStack += [(height, currHeight_end)]
-                endStack += [(-currHeight_end, height, )]
-            # Make sure heightStack is correct
+                stack += [(height, currHeight_end)]
+            # Make sure Stack is correct
 
-            currHeight = heightStack[-1][0] if heightStack else 0
+            currHeight = stack[-1][0] if stack else 0
             while res and res[-1][0] == pos:
                 _, prevHeight = res.pop()
                 currHeight = max(currHeight, prevHeight)
