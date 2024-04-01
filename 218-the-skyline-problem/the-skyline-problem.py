446ddd6246d449494dbf4c9a7f4649ee25970ff0
Xujie Zheng <z374201743@gmail.com>
2024-04-01 14:40:50 -0400

    Time: 164 ms (26.02%) | Memory: 24.4 MB (5.10%) - LeetSync

--- 218-the-skyline-problem/the-skyline-problem.py
+++ 218-the-skyline-problem/the-skyline-problem.py
@@ -20,28 +20,13 @@ class Solution:
                     stack.remove(item)
             else:
                 stack += [(height, key)]
-            # print(pos, key, isEnd, stack)
+            # Make sure Stack is correct
+
             currHeight = stack[-1][0] if stack else 0
-            # [[0,3],[2,0],[2,3],[5,0]]
-            # [[0,3],[5,0]]
-
-            # [[1,1],[1,2],[1,3],[2,2],[2,0]]
-            # [[1,3],[2,0]]
-
-            # [[1,219],[2,228],[19,225],[45,229],[89,190],[95,175],[97,152],[99,145],[99,74],[100,0]]
-            # [[1,219],[2,228],[19,225],[45,229],[89,190],[95,175],[97,152],[99,74],[100,0]]
-            
-        
             while res and res[-1][0] == pos:
                 _, prevHeight = res.pop()
                 currHeight = max(currHeight, prevHeight)
-            # res += [[pos, currHeight]]
-            
-            # while res and res[-1][1] == currHeight:
-            #     prevPos, _ = res.pop()
-            #     pos = min(prevPos, pos)
-
-            # res += [[pos, currHeight]]
+    
             if not res or res[-1][1] != currHeight:
                 res += [[pos, currHeight]]
 
