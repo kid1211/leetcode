bec681ea3781ae99f76d1e1fd0ff6d9b9d274b21
Xujie Zheng <z374201743@gmail.com>
2024-04-01 21:02:16 -0400

    Time: 47 ms (27.95%) | Memory: 16.6 MB (52.83%) - LeetSync

--- 31-next-permutation/next-permutation.py
+++ 31-next-permutation/next-permutation.py
@@ -3,9 +3,8 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        
         # 183265 -> 183526
-        # 右边往左边升序结束的地方, 的前一位和醉生序的开头swap,然后把后面reverse
+        # 右边往左边升序结束的地方, 的前一位和最生序的开头swap,然后把后面reverse
         # 2, 5 swap
         # 183625
 
@@ -13,32 +12,26 @@ class Solution:
         # 18765432 => 2, 1 swap 28765431, -> reversed 8765431
 
         # 3987421 => 4 和3换
-        # 4987321 ==> reverse
-            # 4123789
+        # 4987321 ==> reverse 4123789
         if len(nums) == 1:
             return
 
         n = len(nums)
-        for i in range(n - 2, -1, -1):
-            if nums[i] < nums[i + 1]:
-                break
+        i = n - 2
+        while i >= 0 and nums[i] >= nums[i + 1]:
+            i -= 1
 
-        for j in range(n - 1, -1, -1):
-            if nums[j] > nums[i]:
-                break
-        print(i, j)
-        if i < j:
+        if i >= 0:
+            for j in range(n - 1, -1, -1):
+                if nums[j] > nums[i]:
+                    break
             nums[i], nums[j] = nums[j], nums[i]
-
-        i, j = i + 1 if i < j else 0, n - 1
+        print(nums)
+        i, j = i + 1 if i >= 0 else 0, n - 1
         while i < j:
             nums[i], nums[j] = nums[j], nums[i]
             i += 1
             j -= 1
-
-
-3987421
-2876543
 
 
         
