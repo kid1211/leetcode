fad9fa7228806dd8f053697ee87d7c5af37e4b2b
Xujie Zheng <z374201743@gmail.com>
2024-04-02 19:59:11 -0400

    Time: 497 ms (33.17%) | Memory: 56.4 MB (20.46%) - LeetSync

--- 629-k-inverse-pairs-array/k-inverse-pairs-array.py
+++ 629-k-inverse-pairs-array/k-inverse-pairs-array.py
@@ -7,14 +7,13 @@ class Solution:
                 if j == 0:
                     dp[i][j] = 1
                 else:
-                    val = dp[i - 1][j] % M
+                    val = dp[i - 1][j]
                     if j - i >= 0:
-                        val -= dp[i - 1][j - i] % M
+                        val -= dp[i - 1][j - i]
                     dp[i][j] = dp[i][j - 1] + val
                     dp[i][j] %= M
         
-        res = dp[n][k] % M
+        res = dp[n][k]
         if k > 0:
-            res -= dp[n][k - 1] % M
-
+            res -= dp[n][k - 1]
         return res % M
