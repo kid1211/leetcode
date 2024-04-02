aab01c6a72b4232db953105afd30504b5f9cfe78
Xujie Zheng <z374201743@gmail.com>
2024-04-02 13:29:36 -0400

    Time: 940 ms (53.48%) | Memory: 39.1 MB (69.82%) - LeetSync

--- 833-bus-routes/bus-routes.py
+++ 833-bus-routes/bus-routes.py
@@ -1,10 +1,10 @@
 class Solution:
     def numBusesToDestination(self, routes: List[List[int]], source: int, target: int) -> int:
         n = len(routes)
-        busPath = { i: set(routes[i]) for i in range(len(routes)) }
+        busPath = { i:set(routes[i]) for i in range(n) }
         busPath[-1] = set([source])
-
         edges = defaultdict(set)
+        # -1
         for i in range(n):
             if source in busPath[i]:
                 edges[-1].add(i)
@@ -12,37 +12,22 @@ class Solution:
                 if busPath[i].intersection(busPath[j]):
                     edges[i].add(j)
                     edges[j].add(i)
-        # print(edges)
+        
+        
         queue = deque([-1])
         visited = set([-1])
         res = -1
 
         while queue:
-            # print(queue)
             res += 1
             for _ in range(len(queue)):
-                route = queue.popleft()
+                bus = queue.popleft()
 
-                if target in busPath[route]:
+                if target in busPath[bus]:
                     return res
                 
-                for nxtRoute in edges[route]:
-                    if nxtRoute in visited:
-                        continue
-                    visited.add(nxtRoute)
-                    queue.append(nxtRoute)
-                    
+                for nxtRoute in edges[bus]:
+                    if nxtRoute not in visited:
+                        visited.add(nxtRoute)
+                        queue += [nxtRoute]
         return -1
-
-# 37 -> 43
-# [
-#     0[10,13,22,28,32,35,*43*],
-#     1[2,11,15,25,27],
-#     2[6,13,18,25,42],
-#     3[5,6,20,27,*37*,47],
-#     4[7,11,19,23,35],
-#     5[7,11,17,25,31,*43*,46,48],
-#     6[1,4,10,16,25,26,46],
-#     7[7,11],
-#     8[3,9,19,20,21,24,32,45,46,49],
-#     9[11,41]]
