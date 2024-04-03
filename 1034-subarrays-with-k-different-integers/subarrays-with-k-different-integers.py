d0d3c54ff84ade0b8d9bf6641906890f9ba36d45
Xujie Zheng <z374201743@gmail.com>
2024-04-02 23:29:58 -0400

    Time: 364 ms (22.45%) | Memory: 20 MB (58.26%) - LeetSync

--- 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.py
+++ 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.py
@@ -1,18 +1,22 @@
 class Solution:
     def subarraysWithKDistinct(self, nums: List[int], k: int) -> int:
-        def slidingWindowAtMostK(k):
+        
+        def helper(k):
             if k == 0:
                 return 0
-            left = res = 0
             unique = defaultdict(int)
+            res = left = 0
             for right in range(len(nums)):
                 unique[nums[right]] += 1
-
+                
                 while left < right and len(unique) > k:
                     unique[nums[left]] -= 1
                     if unique[nums[left]] == 0:
                         del unique[nums[left]]
                     left += 1
+                # print(left, right)
                 res += right - left + 1
             return res
-        return slidingWindowAtMostK(k) - slidingWindowAtMostK(k - 1)
+        
+        return helper(k) - helper(k - 1)
+        # return helper(k) #test
