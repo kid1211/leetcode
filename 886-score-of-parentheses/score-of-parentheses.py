cbda8330a49a607ce7839dfd056fd929fe7c3a81
Xujie Zheng <z374201743@gmail.com>
2024-04-07 00:22:57 -0400

    Time: 33 ms (69.04%) | Memory: 16.7 MB (7.47%) - LeetSync

--- 886-score-of-parentheses/score-of-parentheses.py
+++ 886-score-of-parentheses/score-of-parentheses.py
@@ -1,16 +1,15 @@
 class Solution:
     def scoreOfParentheses(self, s: str) -> int:
-        bal = res = 0
-        reset = True
+        res = bal = 0
+        reset = False
         for l in s:
             if l == "(":
                 bal += 1
                 reset = True
             elif l == ")":
                 bal -= 1
-                
                 if reset:
                     res += 2 ** bal
                     reset = False
-
         return res
+
