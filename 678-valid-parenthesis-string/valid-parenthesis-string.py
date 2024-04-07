e150c56f4ee29bdc89756e1fde66403e8aaffd0a
Xujie Zheng <z374201743@gmail.com>
2024-04-06 20:24:27 -0400

    Time: 45 ms (10.82%) | Memory: 17.3 MB (7.80%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.py
+++ 678-valid-parenthesis-string/valid-parenthesis-string.py
@@ -7,8 +7,11 @@ class Solution:
                 return bal == 0
 
             if s[i] == "*":
-                isValid = dfs(i + 1, bal + 1) or dfs(i + 1, bal)
-                return isValid or bal > 0 and dfs(i + 1, bal - 1)
+                return (
+                    dfs(i + 1, bal + 1) or 
+                    dfs(i + 1, bal) or 
+                    bal > 0 and dfs(i + 1, bal - 1)
+                    )
             else:
                 if s[i] == "(":
                     return dfs(i + 1, bal + 1)
@@ -18,28 +21,3 @@ class Solution:
                     return False
         
         return dfs(0, 0)
-
-# class Solution:
-#     def checkValidString(self, s: str) -> bool:
-#         @cache
-#         def is_valid_string(index: int, open_count: int) -> bool:
-#             if index == len(s):
-#                 return open_count == 0
-
-#             is_valid = False
-#             if s[index] == '*':
-#                 is_valid |= is_valid_string(index + 1, open_count + 1)  # Treat '*' as '('
-#                 if open_count > 0:
-#                     is_valid |= is_valid_string(index + 1, open_count - 1)  # Treat '*' as ')'
-#                 is_valid |= is_valid_string(index + 1, open_count)  # Treat '*' as empty
-#             else:
-#                 # Handle '(' and ')'
-#                 if s[index] == '(':
-#                     is_valid = is_valid_string(index + 1, open_count + 1)  # Increment count for '('
-#                 elif open_count > 0:
-#                     is_valid = is_valid_string(index + 1, open_count - 1)  # Decrement count for ')'
-
-#             # Memoize and return the result
-#             # memo[index][open_count] = 1 if is_valid else 0
-#             return is_valid
-#         return is_valid_string(0, 0)
