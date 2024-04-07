dae8e0cf23663086ca5a82e42c4c88a3772b3167
Xujie Zheng <z374201743@gmail.com>
2024-04-07 00:25:41 -0400

    Time: 335 ms (48.19%) | Memory: 19.9 MB (58.22%) - LeetSync

--- 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.py
+++ 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.py
@@ -1,6 +1,5 @@
 class Solution:
     def subarraysWithKDistinct(self, nums: List[int], k: int) -> int:
-        
         def helper(k):
             if k == 0:
                 return 0
@@ -8,15 +7,12 @@ class Solution:
             res = left = 0
             for right in range(len(nums)):
                 unique[nums[right]] += 1
-                
+
                 while left < right and len(unique) > k:
                     unique[nums[left]] -= 1
                     if unique[nums[left]] == 0:
                         del unique[nums[left]]
                     left += 1
-                # print(left, right)
                 res += right - left + 1
             return res
-        
         return helper(k) - helper(k - 1)
-        # return helper(k) #test
