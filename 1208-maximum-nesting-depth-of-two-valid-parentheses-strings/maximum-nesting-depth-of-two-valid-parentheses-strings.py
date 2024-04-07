b230ecaf2b6e78bd513313b788dd9a883a7033b2
Xujie Zheng <z374201743@gmail.com>
2024-04-07 16:49:40 -0400

    Time: 40 ms (79.46%) | Memory: 17.3 MB (41.09%) - LeetSync

--- 1208-maximum-nesting-depth-of-two-valid-parentheses-strings/maximum-nesting-depth-of-two-valid-parentheses-strings.py
+++ 1208-maximum-nesting-depth-of-two-valid-parentheses-strings/maximum-nesting-depth-of-two-valid-parentheses-strings.py
@@ -8,13 +8,12 @@ class Solution:
         for i in range(n):
             if seq[i] == "(":
                 stack += [i]
-                belong = 1 - belong # hmm
             else:
                 if not stack:
                     return [] # not valid VPS
                 res[i] = belong
                 res[stack.pop()] = belong
-                belong = 1 - belong
+            belong = 1 - belong
         return res
 
             
