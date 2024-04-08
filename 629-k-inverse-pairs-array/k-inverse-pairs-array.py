f5dbb6bf84cdd39b8d028d2c718c1a5884756944
Xujie Zheng <z374201743@gmail.com>
2024-04-08 10:31:16 -0400

    Time: 2038 ms (5.88%) | Memory: 777.4 MB (5.01%) - LeetSync

--- 629-k-inverse-pairs-array/k-inverse-pairs-array.py
+++ 629-k-inverse-pairs-array/k-inverse-pairs-array.py
@@ -1,6 +1,9 @@
 class Solution:
     def kInversePairs(self, n: int, k: int) -> int:
+        # 123 => 4123 # 0 + 3
+        # 132 => 4132 # 1 + 3
         M = 10**9 + 7
+
         @cache
         def dfs(n, k):
             if k < 0 or n == 0:
@@ -10,7 +13,8 @@ class Solution:
             
             return (
                 dfs(n, k - 1) +
-                dfs(n - 1, k) - dfs(n - 1, k - n)
+                dfs(n - 1, k) -
+                dfs(n - 1, k - n)
             ) % M
-        
+
         return (dfs(n, k) - dfs(n, k - 1)) % M
