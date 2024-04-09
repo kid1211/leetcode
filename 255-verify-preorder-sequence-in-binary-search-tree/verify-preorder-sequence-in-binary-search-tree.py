d2be71cdee21c21933df26a57cbbf2f618c7f7d0
Xujie Zheng <z374201743@gmail.com>
2024-04-09 18:36:09 -0400

    Time: 178 ms (32.91%) | Memory: 17.8 MB (25.88%) - LeetSync

--- 255-verify-preorder-sequence-in-binary-search-tree/verify-preorder-sequence-in-binary-search-tree.py
+++ 255-verify-preorder-sequence-in-binary-search-tree/verify-preorder-sequence-in-binary-search-tree.py
@@ -1,18 +1,13 @@
 class Solution:
     def verifyPreorder(self, preorder: List[int]) -> bool:
-        i = 0
-        def helper(min_limit, max_limit):
-            nonlocal i
-            if i == len(preorder):
-                return True
-            
-            root = preorder[i]
-            if not min_limit < root < max_limit:
-                return False
+        min_limit = -inf
+        stack = []
 
-            i += 1
-            left = helper(min_limit, root)
-            right = helper(root, max_limit)
-            return left or right
+        for num in preorder:
+            while stack and stack[-1] < num:
+                min_limit = stack.pop()
             
-        return helper(-inf, inf)
+            if num <= min_limit:
+                return False
+            stack += [num]
+        return True
