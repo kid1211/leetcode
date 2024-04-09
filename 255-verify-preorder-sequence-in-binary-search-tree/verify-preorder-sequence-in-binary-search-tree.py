3feb80c525b7b00ae0aa5361a2f0c4d8379cd982
Xujie Zheng <z374201743@gmail.com>
2024-04-09 18:24:00 -0400

    Time: 205 ms (9.00%) | Memory: 23.8 MB (9.71%) - LeetSync

--- 255-verify-preorder-sequence-in-binary-search-tree/verify-preorder-sequence-in-binary-search-tree.py
+++ 255-verify-preorder-sequence-in-binary-search-tree/verify-preorder-sequence-in-binary-search-tree.py
@@ -1,16 +1,18 @@
 class Solution:
     def verifyPreorder(self, preorder: List[int]) -> bool:
-        def helper(i, min_limit, max_limit):
-            if i[0] == len(preorder):
+        i = 0
+        def helper(min_limit, max_limit):
+            nonlocal i
+            if i == len(preorder):
                 return True
             
-            root = preorder[i[0]]
+            root = preorder[i]
             if not min_limit < root < max_limit:
                 return False
 
-            i[0] += 1
-            left = helper(i, min_limit, root)
-            right = helper(i, root, max_limit)
+            i += 1
+            left = helper(min_limit, root)
+            right = helper(root, max_limit)
             return left or right
             
-        return helper([0], -inf, inf)
+        return helper(-inf, inf)
