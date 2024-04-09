b4c802b0da4b3663793777d670c9da5b0dbad08b
Xujie Zheng <z374201743@gmail.com>
2024-04-09 19:53:54 -0400

    Time: 1170 ms (54.71%) | Memory: 66.6 MB (76.29%) - LeetSync

--- 2364-longest-path-with-different-adjacent-characters/longest-path-with-different-adjacent-characters.py
+++ 2364-longest-path-with-different-adjacent-characters/longest-path-with-different-adjacent-characters.py
@@ -1,33 +1,28 @@
 class Solution:
     def longestPath(self, parent: List[int], s: str) -> int:
-        longestPath = 1
+        edges = defaultdict(list)
         n = len(parent)
-        children = defaultdict(list)
+        for i in range(n):
+            edges[parent[i]] += [i]
+        
+        res = 0
+        def dfs(curr):
+            nonlocal res
+            
+            longest = secondLongest = 0
+            for child in edges[curr]:
+                longestChain = dfs(child)
 
-        for i in range(1, n):
-            children[parent[i]] += [i]
-
-        def dfs(currentNode):
-            nonlocal longestPath
-            if currentNode not in children:
-                return 1
-            longestChain = secondLongestchain = 0
-            for child in children[currentNode]:
-                longestChainStartingFromChild = dfs(child)
-
-                if s[currentNode] == s[child]:
+                if s[curr] == s[child]:
                     continue
                 
-                if longestChainStartingFromChild > longestChain:
-                    secondLongestchain = longestChain
-                    longestChain = longestChainStartingFromChild
-                elif longestChainStartingFromChild > secondLongestchain:
-                    secondLongestchain = longestChainStartingFromChild
-            longestPath = max(longestPath, longestChain + secondLongestchain + 1)
-            return longestChain + 1
-
-        dfs(0)
-        return longestPath
-
-# defaultdict(<class 'list'>, {-1: [0], 0: [1, 2, 3]})
-# defaultdict(<class 'list'>, {-1: [0], 0: [1, 2], 1: [3, 4], 2: [5]})
+                if longestChain > longest:
+                    longest, secondLongest = longestChain, longest
+                elif longestChain > secondLongest:
+                    secondLongest = longestChain
+                
+            res = max(res, longest + secondLongest + 1)
+            return longest + 1
+        
+        dfs(edges[-1][0])
+        return res
