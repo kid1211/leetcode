62b6ce36c6e36240717146e67ef78740584ab2f3
Xujie Zheng <z374201743@gmail.com>
2024-04-09 10:01:37 -0400

    Time: 79 ms (29.65%) | Memory: 20.7 MB (7.38%) - LeetSync

--- 253-meeting-rooms-ii/meeting-rooms-ii.py
+++ 253-meeting-rooms-ii/meeting-rooms-ii.py
@@ -1,13 +1,12 @@
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
         pts = []
-
         for start, end in intervals:
             pts += [(start, 1)]
             pts += [(end, -1)]
         
-        res = rolling = 0
+        res = curr = 0
         for _, delta in sorted(pts):
-            rolling += delta
-            res = max(res, rolling)
+            curr += delta
+            res = max(res, curr)
         return res
