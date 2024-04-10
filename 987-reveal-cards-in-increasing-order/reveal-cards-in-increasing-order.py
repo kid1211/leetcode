bd06fdfca043404428f1fb567ffea8fa5eac8ac5
Xujie Zheng <z374201743@gmail.com>
2024-04-09 23:18:52 -0400

    Time: 46 ms (57.45%) | Memory: 16.8 MB (80.95%) - LeetSync

---
+++ 987-reveal-cards-in-increasing-order/reveal-cards-in-increasing-order.py
@@ -0,0 +1,25 @@
+class Solution:
+    def deckRevealedIncreasing(self, deck: List[int]) -> List[int]:
+        # [2, 3, 5, 7, 11, 13, 17]
+        # 2,13,3,11,5,17,7
+        
+        # [13, 17] # 13 -> [17]
+        # [11, 17, 13] # 11 -> [13, 17]
+        # [7, 13, 11, 17]. # 7 -> [11, 17, 13]
+        # # move last up
+        # [5, 17, 7, 13, 11]
+        # [3, 11, 5, 17, 7, 13]
+        # [2, 13, 3, 11, 5, 17, 7]
+        deck.sort(reverse=True)
+        res = deque()
+        for num in deck:
+            if not res:
+                res += [num]
+            elif len(res) == 1:
+                res.appendleft(num)
+            else:
+                right = res.pop()
+                res.appendleft(right)
+                res.appendleft(num)
+        
+        return list(res)
