832731934c595351d9baa1fb959b454b3f11222f
Xujie Zheng <z374201743@gmail.com>
2024-04-09 20:02:57 -0400

    Time: 187 ms (18.85%) | Memory: 23.9 MB (6.33%) - LeetSync

--- 255-verify-preorder-sequence-in-binary-search-tree/verify-preorder-sequence-in-binary-search-tree.py
+++ 255-verify-preorder-sequence-in-binary-search-tree/verify-preorder-sequence-in-binary-search-tree.py
@@ -1,15 +1,18 @@
 class Solution:
     def verifyPreorder(self, preorder: List[int]) -> bool:
-        def helper(i, min_limit, max_limit):
-            if i == len(preorder):
-                return i, True
-            
-            root = preorder[i]
-            if not min_limit < root < max_limit:
-                return i, False
+        
+        def dfs(curr, mini, maxi):
+            if curr >= len(preorder):
+                return curr, True
+            # exit?
 
-            leftIdx, left = helper(i + 1, min_limit, root)
-            rightIdx, right = helper(leftIdx, root, max_limit)
-            return rightIdx, left or right
-            
-        return helper(0, -inf, inf)[1]
+            root = preorder[curr]
+            if not mini <= root <= maxi:
+                return curr, False
+
+            leftEnd, left = dfs(curr + 1, mini, root)
+            rightEnd, right = dfs(leftEnd, root, maxi)
+
+            return rightEnd, left or right
+        
+        return dfs(0, -sys.maxsize, sys.maxsize)[1]
