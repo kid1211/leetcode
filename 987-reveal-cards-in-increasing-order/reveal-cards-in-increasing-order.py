060cfc4b08a86fbf06e370d34ab5424c1bed4d77
Xujie Zheng <z374201743@gmail.com>
2024-04-09 23:21:10 -0400

    Time: 47 ms (48.83%) | Memory: 16.8 MB (36.98%) - LeetSync

--- 987-reveal-cards-in-increasing-order/reveal-cards-in-increasing-order.py
+++ 987-reveal-cards-in-increasing-order/reveal-cards-in-increasing-order.py
@@ -13,13 +13,16 @@ class Solution:
         deck.sort(reverse=True)
         res = deque()
         for num in deck:
-            if not res:
-                res += [num]
-            elif len(res) == 1:
-                res.appendleft(num)
-            else:
-                right = res.pop()
-                res.appendleft(right)
-                res.appendleft(num)
+            # if not res:
+            #     res += [num]
+            # elif len(res) == 1:
+            #     res.appendleft(num)
+            # else:
+            #     right = res.pop()
+            #     res.appendleft(right)
+            #     res.appendleft(num)
+            if len(res) > 1:
+                res.appendleft(res.pop())
+            res.appendleft(num)
         
         return list(res)
