b8bd7d402beb135460a8b1138e9cefb1ad8f4cea
Xujie Zheng <z374201743@gmail.com>
2024-04-14 23:49:04 -0400

    Time: 229 ms (38.65%) | Memory: 17.8 MB (76.10%) - LeetSync

--- 85-maximal-rectangle/maximal-rectangle.py
+++ 85-maximal-rectangle/maximal-rectangle.py
@@ -1,27 +1,21 @@
 class Solution:
     def maximalRectangle(self, matrix: List[List[str]]) -> int:
-        if not matrix:
-            return 0
-
         def helper(nums):
+            nonlocal res
             nums += [0]
             stack = []
-            res = 0
             for i in range(len(nums)):
                 while stack and nums[stack[-1]] > nums[i]:
                     height = nums[stack.pop()]
                     left = stack[-1] if stack else -1
                     res = max(res, height * (i - left - 1))
                 stack += [i]
-            return res
-
-
         
         rows, cols = len(matrix), len(matrix[0])
-        dp = [0] * cols
         res = 0
+        dp = [0] * cols
         for x in range(rows):
             for y in range(cols):
                 dp[y] = dp[y] + 1 if matrix[x][y] == "1" else 0
-            res = max(res, helper(dp))
+            helper(dp)
         return res
