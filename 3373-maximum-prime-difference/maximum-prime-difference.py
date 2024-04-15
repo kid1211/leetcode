af1b92f23b6f3af77c5799c0003cc46ba3832835
Xujie Zheng <z374201743@gmail.com>
2024-04-15 13:27:48 -0400

    Time: 689 ms (100.00%) | Memory: 25.8 MB (100.00%) - LeetSync

---
+++ 3373-maximum-prime-difference/maximum-prime-difference.py
@@ -0,0 +1,22 @@
+from math import sqrt
+class Solution:
+    def maximumPrimeDifference(self, nums: List[int]) -> int:
+        def Prime(n):
+            if n <= 1:
+                return False
+
+            for i in range(2, int(sqrt(n)) + 1):
+                if (n % i == 0):
+                    return False
+            return True
+    
+        left, right = 0, len(nums) - 1
+        while left < right and not Prime(nums[right]):
+            right -= 1
+        while left < right and not Prime(nums[left]):
+            left += 1
+        
+        if left <= right and Prime(nums[right]) and Prime(nums[left]):
+            return right - left
+
+        return 0
