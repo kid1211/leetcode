ed0c9d638eb02f0d239378c7608827d261265231
Xujie Zheng <z374201743@gmail.com>
2024-04-15 21:02:35 -0400

    Time: 316 ms (50.22%) | Memory: 16.5 MB (45.05%) - LeetSync

--- 465-optimal-account-balancing/optimal-account-balancing.py
+++ 465-optimal-account-balancing/optimal-account-balancing.py
@@ -1,25 +1,27 @@
 class Solution:
     def minTransfers(self, transactions: List[List[int]]) -> int:
-        balance = collections.defaultdict(int)
+        bal = defaultdict(int)
 
         for a, b, amount in transactions:
-            balance[a] -= amount
-            balance[b] += amount
-
-        vals = [val for val in balance.values() if val != 0]
+            bal[a] += amount
+            bal[b] -= amount
         
-        # @cache
+        val = [item for item in bal.values() if item != 0]
+        
         def dfs(curr):
-            while curr < len(vals) and vals[curr] == 0:
+            while curr < len(val) and val[curr] == 0:
                 curr += 1
-            if curr == len(vals):
+    
+            if curr == len(val):
                 return 0
-
+            
             res = sys.maxsize
-            for nxt in range(curr + 1, len(vals)):
-                if vals[nxt] * vals[curr] < 0:
-                    vals[nxt] += vals[curr]
+            for nxt in range(curr + 1, len(val)):
+                if val[nxt] * val[curr] < 0:
+                    val[nxt] += val[curr]
+                    # val[curr] -= val[nxt]
                     res = min(res, 1 + dfs(curr + 1))
-                    vals[nxt] -= vals[curr]
+                    val[nxt] -= val[curr]
+                    # val[curr] += val[nxt]
             return res
         return dfs(0)
