93d10084bbbec4a53259c5c7861f00a7e29c6854
Xujie Zheng <z374201743@gmail.com>
2024-04-20 14:49:02 -0400

    Time: 1188 ms (6.36%) | Memory: 26.3 MB (48.37%) - LeetSync

--- 2155-find-missing-observations/find-missing-observations.py
+++ 2155-find-missing-observations/find-missing-observations.py
@@ -1,15 +1,25 @@
 class Solution:
     def missingRolls(self, rolls: List[int], mean: int, n: int) -> List[int]:
-        curr_total = sum(rolls)
-        total = (len(rolls) + n) * mean
+        total = sum(rolls)
+        remain = mean * (n + len(rolls)) - total
 
-        remain = total - curr_total
-        if not (1 <= remain / n <= 6):
+        if not (1 <= remain/n <= 6):
             return []
-
+        print(remain, n)
+        # [6, 6, 6, 1, 1]
+        # 12 + 15
         res = []
-        for i in range(n):
-            val = min(6, remain - (n - i - 1))
-            res += [val]
+
+        while n - len(res):
+            # n - len(res) == remain slot if filled all 1
+            val = min(6, remain - (n - len(res) - 1))
             remain -= val
+            res += [val]
+        
         return res
+# 3
+# 3 - 2
+
+# 12/2 
+# 6, 6
+# 9 - 6 = 3, < 2 - 1 - 1
