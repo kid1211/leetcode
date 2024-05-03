16ed25764b6c3f8621435b61459275038e39254f
Xujie Zheng <z374201743@gmail.com>
2024-05-03 12:35:48 -0400

    Time: 39 ms (25.60%) | Memory: 16.5 MB (91.99%) - LeetSync

--- 165-compare-version-numbers/compare-version-numbers.py
+++ 165-compare-version-numbers/compare-version-numbers.py
@@ -7,7 +7,7 @@ class Solution:
                 if text[end] == ".":
                     break
             end += 1 if end == len(text) - 1 else 0
-            return text[start:end], end + 1
+            return int(text[start:end]) if text[start:end] else None, end + 1
     
         leftIdx = rightIdx = 0
         while True:
@@ -15,25 +15,25 @@ class Solution:
             right, rightIdx = getNext(version2, rightIdx)
 
             # print(leftIdx, rightIdx, left, right)
-            if left and right:
-                v1 = int(left)
-                v2 = int(right)
+            if left is not None and right is not None:
+                # v1 = int(left)
+                # v2 = int(right)
                 # print('ans', v1, v2)
-                if v1 < v2:
+                if left < right:
                     return -1
-                elif v1 > v2:
+                elif left > right:
                     return 1
             else:
                 break
 
         print('end', leftIdx, rightIdx)
-        while left:
-            if int(left) != 0:
+        while left is not None:
+            if left:
                 return 1
             left, leftIdx = getNext(version1, leftIdx)
     
-        while right:
-            if int(right) != 0:
+        while right is not None:
+            if right:
                 return -1
             right, rightIdx = getNext(version2, rightIdx)
 
