d81db433fea457ba5217d0037536548c5479926b
Xujie Zheng <z374201743@gmail.com>
2024-05-06 23:43:03 -0400

    Time: 497 ms (26.09%) | Memory: 51.2 MB (78.26%) - LeetSync

--- 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.py
+++ 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.py
@@ -1,42 +1,34 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
-    def reverse_list(self, head):
-        prev = None
-        current = head
-        next_temp = None
-
-        # Set each node's next pointer to the previous node
-        while current:
-            next_temp = current.next
-            current.next = prev
-            prev = current
-            current = next_temp
-        
-        return prev
-
     def removeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        # Reverse the original linked list
-        head = self.reverse_list(head)
 
-        maximum = 0
-        prev = None
-        current = head
+        def reverse(node):
+            last = None
+            while node:
+                nxt = node.next
+                node.next = last
+                last = node
+                node = nxt
+            return last
 
-        # Traverse the list deleting nodes
-        while current:
-            maximum = max(maximum, current.val)
+        def skip(node):
+            last, curr = None, node
+            maxi = 0
+            while curr:
+                maxi = max(maxi, curr.val)
 
-            # Delete nodes that are smaller than maximum
-            if current.val < maximum:
-                # Delete current by skipping
-                prev.next = current.next
-                deleted = current
-                current = current.next
-                deleted.next = None
-
-            # Current does not need to be deleted
-            else:
-                prev = current
-                current = current.next
-        
-        # Reverse and return the modified linked list
-        return self.reverse_list(head)
+                if curr.val < maxi:
+                    last.next = curr.next
+                    nxt = curr.next
+                    # curr.next = None
+                    curr = nxt
+                else:
+                    last = curr
+                    curr = curr.next
+        newHead = reverse(head)
+        skip(newHead)
+        return reverse(newHead)
