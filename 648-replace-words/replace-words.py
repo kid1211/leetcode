7a34a0f7f7aabe8ede11cbec861a5e1b537fb45d
Xujie Zheng <z374201743@gmail.com>
2024-06-07 14:13:11 -0400

    Time: 85 ms (90.43%) | Memory: 32.8 MB (48.09%) - LeetSync

---
+++ 648-replace-words/replace-words.py
@@ -0,0 +1,41 @@
+class Solution:
+    def replaceWords(self, dictionary: List[str], sentence: str) -> str:
+        trie = Trie(dictionary)
+
+        res = []
+        for word in sentence.split():
+            prefix = trie.prefix(word)
+            res += [prefix] if prefix else [word]
+        return " ".join(res)
+
+class TrieNode:
+    def __init__(self):
+        self.word = ""
+        self.children = {}
+
+class Trie:
+    def __init__(self, dict):
+        self.root = TrieNode()
+        for word in dict:
+            self.add(word)
+
+    def add(self, word):
+        node = self.root
+
+        for l in word:
+            if node.word:
+                print(node.word, l)
+                return
+            if l not in node.children:
+                node.children[l] = TrieNode()
+            node = node.children[l]
+        node.word = word
+        node.children = {}
+
+    def prefix(self, prefix):
+        node = self.root
+        for l in prefix:
+            if l not in node.children:
+                break
+            node = node.children[l]
+        return node.word if node.word else None
