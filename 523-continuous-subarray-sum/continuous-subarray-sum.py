3d2de926aa71c43b3b0d7b48af711afeaebfcacd
Xujie Zheng <z374201743@gmail.com>
2024-06-07 20:53:35 -0400

    Time: 836 ms (39.58%) | Memory: 36.1 MB (84.70%) - LeetSync

--- 523-continuous-subarray-sum/continuous-subarray-sum.py
+++ 523-continuous-subarray-sum/continuous-subarray-sum.py
@@ -1,14 +1,17 @@
 class Solution:
     def checkSubarraySum(self, nums: List[int], k: int) -> bool:
         remain = {}
-        remain[0] = -1
         rolling = 0
+        remain[0] = -1
 
         for i in range(len(nums)):
             rolling += nums[i]
+
             if rolling % k in remain:
-                if i - remain[rolling % k] > 1:
+                last = remain[rolling % k]
+                if i - last > 1:
                     return True
             else:
                 remain[rolling % k] = i
         return False
+
