d2b9428810642708c04a8715c035cb9e8ce5c65d
Xujie Zheng <z374201743@gmail.com>
2024-07-05 23:18:09 -0400

    Time: 31 ms (77.47%) | Memory: 16.5 MB (30.38%) - LeetSync

---
+++ 2645-pass-the-pillow/pass-the-pillow.py
@@ -0,0 +1,12 @@
+class Solution:
+    def passThePillow(self, n: int, time: int) -> int:
+        l2r = True
+
+        while time:
+            if n > time:
+                break
+            time -= (n - 1)
+            l2r = not l2r
+        
+        print(l2r, time)
+        return time + 1 if l2r else n - time
