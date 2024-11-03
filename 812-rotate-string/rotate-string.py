a805b392c91cdd30533de1c6167d9e326b7a84c7
Xujie Zheng <z374201743@gmail.com>
2024-11-03 14:38:32 -0500

    Time: 0 ms (100.00%) | Memory: 16.5 MB (40.94%) - LeetSync

--- 812-rotate-string/rotate-string.py
+++ 812-rotate-string/rotate-string.py
@@ -4,30 +4,3 @@ class Solution:
             return False
         s += s
         return goal in s
-        # expected = actual = 0
-        # PRIME = 31
-
-        # def get(l):
-        #     return ord(l) - ord('a')
-        
-
-        # def add(val, l):
-        #     val *= PRIME
-        #     val += get(l)
-        #     val %= sys.maxsize // PRIME
-        #     return val
-
-        
-        # for i in range(len(goal)):
-        #     expected = add(expected, goal[i])
-        #     actual = add(actual, s[i])
-        
-        # coef = PRIME ** (len(goal) - 1)
-        # for i in range(len(goal), len(s)):
-        #     if expected == actual:
-        #         return True
-            
-        #     actual -= coef * get(s[i - len(goal)])
-        #     actual = add(actual, s[i])
-            
-        # return expected == actual
