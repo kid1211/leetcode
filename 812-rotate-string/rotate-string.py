0967a17f445fdf42523ecb58fa80129e96aef37b
Xujie Zheng <z374201743@gmail.com>
2024-11-02 20:40:32 -0400

    Time: 0 ms (100.00%) | Memory: 16.6 MB (9.22%) - LeetSync

--- 812-rotate-string/rotate-string.py
+++ 812-rotate-string/rotate-string.py
@@ -3,30 +3,31 @@ class Solution:
         if len(s) != len(goal):
             return False
         s += s
-        expected = actual = 0
-        PRIME = 31
+        return goal in s
+        # expected = actual = 0
+        # PRIME = 31
 
-        def get(l):
-            return ord(l) - ord('a')
+        # def get(l):
+        #     return ord(l) - ord('a')
         
 
-        def add(val, l):
-            val *= PRIME
-            val += get(l)
-            val %= sys.maxsize // PRIME
-            return val
+        # def add(val, l):
+        #     val *= PRIME
+        #     val += get(l)
+        #     val %= sys.maxsize // PRIME
+        #     return val
 
         
-        for i in range(len(goal)):
-            expected = add(expected, goal[i])
-            actual = add(actual, s[i])
+        # for i in range(len(goal)):
+        #     expected = add(expected, goal[i])
+        #     actual = add(actual, s[i])
         
-        coef = PRIME ** (len(goal) - 1)
-        for i in range(len(goal), len(s)):
-            if expected == actual:
-                return True
+        # coef = PRIME ** (len(goal) - 1)
+        # for i in range(len(goal), len(s)):
+        #     if expected == actual:
+        #         return True
             
-            actual -= coef * get(s[i - len(goal)])
-            actual = add(actual, s[i])
+        #     actual -= coef * get(s[i - len(goal)])
+        #     actual = add(actual, s[i])
             
-        return expected == actual
+        # return expected == actual
