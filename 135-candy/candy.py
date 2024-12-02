06c199c293d62bae76453ed43e0c50c198479e03
Xujie Zheng <z374201743@gmail.com>
2024-12-01 23:39:53 -0500

    Time: 19 ms (33.54%) | Memory: 19.3 MB (22.94%) - LeetSync

--- 135-candy/candy.py
+++ 135-candy/candy.py
@@ -6,35 +6,13 @@
 # I think this is it, do you understand what I am saying? if so, summarized all the things in this sesession, and explain to me why we count inc first then dec
 class Solution:
     def candy(self, ratings: List[int]) -> int:
-        def count(start, end, n):
-            return(n*(start + end))//2
-        def count_zero_based(n):
-            return count(0, n - 1, n)
-        
-        # do the same question in 0 based first
-        # then add the # of children to satisfy the quesiton
-
-        children = len(ratings)
-        candies, i = 0, 1
-        while i < children:
-            inc = dec = 0
-
-            while i < children and ratings[i] > ratings[i-1]:
-                inc += 1
-                i += 1
-
-            while i < children and ratings[i] < ratings[i-1]:
-                dec += 1
-                i += 1
-                
-            # print(inc, dec, i, candies)
-            if inc or dec:
-                # don't know inc is longer or dec is longer, so just add up to inc or dec, 
-                # then add either inc or dec
-                candies += count_zero_based(inc) + max(inc, dec) + count_zero_based(dec)
-                # print(count_zero_based(inc), max(inc, dec), count_zero_based(dec))
-            else:
-                # if it is neither, then adding one will be sufficint which will be cover in candies + chilren
-                # turning 0 based to 1 based
-                i += 1
-        return candies + children
+        if not ratings:
+            return 0
+        ret = [1 for _ in range(len(ratings))]
+        for i in range(1, len(ratings)):
+            if ratings[i] > ratings[i - 1]:
+                ret[i] = ret[i - 1] + 1
+        for i in range(len(ratings) - 2, -1, -1):
+            if ratings[i] > ratings[i + 1]:
+                ret[i] = max(ret[i], ret[i + 1] + 1)
+        return sum(ret)
