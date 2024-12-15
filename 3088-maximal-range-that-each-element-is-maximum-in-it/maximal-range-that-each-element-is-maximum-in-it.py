cfc7c59b59cd1f09a3c76c6d9de8df947461a048
Xujie Zheng <z374201743@gmail.com>
2024-12-14 19:56:20 -0500

    Time: 240 ms (54.17%) | Memory: 39 MB (58.00%) - LeetSync

---
+++ 3088-maximal-range-that-each-element-is-maximum-in-it/maximal-range-that-each-element-is-maximum-in-it.py
@@ -0,0 +1,30 @@
+class Solution:
+    def maximumLengthOfRanges(self, nums):
+        n = len(nums)
+        left = [0] * n
+        right = [0] * n
+        idx_stack = []
+
+        # Calculate left boundaries
+        for curr_idx in range(n):
+            while idx_stack and nums[idx_stack[-1]] < nums[curr_idx]:
+                idx_stack.pop()
+            left[curr_idx] = -1 if not idx_stack else idx_stack[-1]
+            idx_stack.append(curr_idx)
+
+        # Clear the stack for reuse
+        idx_stack = []
+
+        # Calculate right boundaries
+        for curr_idx in range(n - 1, -1, -1):
+            while idx_stack and nums[idx_stack[-1]] < nums[curr_idx]:
+                idx_stack.pop()
+            right[curr_idx] = n if not idx_stack else idx_stack[-1]
+            idx_stack.append(curr_idx)
+
+        # Calculate the maximal range for each element
+        ans = [0] * n
+        for curr_idx in range(n):
+            ans[curr_idx] = right[curr_idx] - left[curr_idx] - 1
+
+        return ans
