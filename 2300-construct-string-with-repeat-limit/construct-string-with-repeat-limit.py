78878fcca49e5dcbe62bc10dc066a4a256923c49
Xujie Zheng <z374201743@gmail.com>
2024-12-16 21:48:48 -0500

    Time: 135 ms (97.10%) | Memory: 18.7 MB (53.15%) - LeetSync

---
+++ 2300-construct-string-with-repeat-limit/construct-string-with-repeat-limit.py
@@ -0,0 +1,54 @@
+class Node:
+    def __init__(self, letter, count):
+        self.letter = letter
+        self.count = count
+        self.next = None
+class Solution:
+    def repeatLimitedString(self, s: str, repeatLimit: int) -> str:
+        counter = Counter(s)
+        last = None
+        head = None
+        for l in reversed("abcdefghijklmnopqrstuvwxyz"):
+            if l not in counter:
+                continue
+            curr = Node(l, counter[l])
+
+            if last:
+                last.next = curr
+            if not head:
+                head = curr
+            last = curr
+        
+        res = ""
+        last = None
+        while head:
+            if last:
+                lastL, lastC = last
+                res += lastL * min(lastC, repeatLimit)
+                
+                if lastC > repeatLimit:
+                    last = (lastL, lastC - repeatLimit)
+                else:
+                    # might not be needed
+                    last = None
+                
+            if last:
+                res += head.letter
+                head.count -= 1
+                if head.count <= 0:
+                    head = head.next
+            else:
+                last = (head.letter, head.count)
+                head = head.next
+        
+        if last:
+            lastL, lastC = last
+            res += lastL * min(lastC, repeatLimit)
+            
+            if lastC > repeatLimit:
+                last = (lastL, lastC - repeatLimit)
+            else:
+                # might not be needed
+                last = None
+        return res
+            
