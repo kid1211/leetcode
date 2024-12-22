92b6dbc117146c081cc2610407233abfae51eb87
Xujie Zheng <z374201743@gmail.com>
2024-12-22 17:39:01 -0500

    Time: 479 ms (70.83%) | Memory: 46.4 MB (9.37%) - LeetSync

--- 3181-find-building-where-alice-and-bob-can-meet/find-building-where-alice-and-bob-can-meet.py
+++ 3181-find-building-where-alice-and-bob-can-meet/find-building-where-alice-and-bob-can-meet.py
@@ -17,12 +17,13 @@ class Solution:
         def build_answer(i):
             mono_stack_size = len(mono_stack)
             for a, b in new_queries[i]:
-                position = findSmallestElementThatIsGreatherThan(a) 
-                if position < mono_stack_size and position >= 0:
+                position = findLeftMostElementThatIsGreatherThan(a) 
+                if position >= 0:
                     result[b] = mono_stack[position][1]
     
         # find the smallest element that is greater than height
-        def findSmallestElementThatIsGreatherThan(height):
+        # 7, 5, 5, 5, 2 => 5
+        def findLeftMostElementThatIsGreatherThan(height):
             if not mono_stack:
                 return -1
     
@@ -30,7 +31,6 @@ class Solution:
             ans = -1
             while left + 1 < right:
                 mid = (left + right) // 2
-
                 if mono_stack[mid][0] <= height:
                     right = mid
                 else:
@@ -43,19 +43,6 @@ class Solution:
             else:
                 return -1
 
-        def search(height):
-            left = 0
-            right = len(mono_stack) - 1
-            ans = -1
-            while left <= right:
-                mid = (left + right) // 2
-                if mono_stack[mid][0] > height:
-                    ans = max(ans, mid)
-                    left = mid + 1
-                else:
-                    right = mid - 1
-            print(mono_stack, ans, height)
-            return ans
     
         mono_stack = [] # ?? largest to smallest 7, 2
         for i in range(len(heights) - 1, -1, -1):
