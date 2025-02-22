0aaf30903f14a0603013b5805d70305faf5e4bcc
Xujie Zheng <z374201743@gmail.com>
2025-02-22 17:04:19 -0500

    Time: 5 ms (94.37%) | Memory: 17.7 MB (90.14%) - LeetSync

---
+++ 3030-find-the-k-th-lucky-number/find-the-k-th-lucky-number.py
@@ -0,0 +1,12 @@
+class Solution:
+    def kthLuckyNumber(self, k: int) -> str:
+        # Increment k to account for 1-based indexing
+        k = k + 1
+
+        # Convert k to a binary string (up to the most significant '1')
+        kth_lucky_num = bin(k)[3:]
+
+        # Replace '0' with '4' and '1' with '7' in the binary string
+        kth_lucky_num = kth_lucky_num.replace("0", "4").replace("1", "7")
+
+        return kth_lucky_num
