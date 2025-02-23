7dd4497e4cd15c5e4fb5d1e226623d3f0e318886
Xujie Zheng <z374201743@gmail.com>
2025-02-22 22:12:31 -0500

    Time: 0 ms (100.00%) | Memory: 18 MB (28.31%) - LeetSync

---
+++ 925-construct-binary-tree-from-preorder-and-postorder-traversal/construct-binary-tree-from-preorder-and-postorder-traversal.py
@@ -0,0 +1,49 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def constructFromPrePost(self, preorder: List[int], postorder: List[int]) -> Optional[TreeNode]:
+        if not preorder:
+            return None
+        
+        if len(preorder) == 1:
+            return TreeNode(preorder[0])
+        
+        if len(preorder) == 2:
+            left = TreeNode(preorder[1])
+            return TreeNode(preorder[0], left)
+        
+        # if len(preorder) == 3:
+        #     if preorder[1:] == postorder[:2]:
+        #         left = TreeNode(preorder[1])
+        #         right = TreeNode(preorder[2])
+        #     else:
+        #         deeperLeft = TreeNode(preorder[2])
+        #         left = TreeNode(preorder[1], deeperLeft)
+        #         right = None
+        #     return TreeNode(preorder[0], left, right)
+
+        leftChildLen = None
+        for i in range(len(postorder) - 1):
+            if postorder[i] == preorder[1]:
+                leftChildLen = i + 1
+        
+        if leftChildLen is None:
+            print('should never happen', preorder, leftChildLen)
+            return None
+        left = self.constructFromPrePost(
+            preorder[1:leftChildLen + 1],
+            postorder[:leftChildLen]
+        )
+        right = self.constructFromPrePost(
+            preorder[1 + leftChildLen:], 
+            postorder[leftChildLen:len(postorder) - 1]
+        )
+        return TreeNode(preorder[0], left, right)
+# [2,1,3]
+# [3,1,2] only if 1, 3, 2
+
+# [2,1,null,3]
