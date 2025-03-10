c05f850044e6ca7645d6cbc11927e13961da8be4
Xujie Zheng <z374201743@gmail.com>
2025-03-10 15:46:19 -0400

    Time: 598 ms (5.03%) | Memory: 30.5 MB (35.88%) - LeetSync

--- 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.py
+++ 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.py
@@ -3,13 +3,19 @@ class Solution:
         counter = Counter(nums)
         res = 0
         for num in nums:
-            if k - num == num and num in counter:
-                if counter[num] >= 2:
-                    res += 1
-                    counter[num] -= 2
-                    if counter[num] == 0:
-                        del counter[num]
-            elif k - num in counter and num in counter:
+            cond1 = k - num == num and num in counter and counter[num] >= 2
+            cond2 = k - num in counter and num in counter
+            if (
+                k - num == num and num in counter and counter[num] >= 2 or
+                k - num != num and k - num in counter and num in counter
+                ):
+            # if k - num == num and num in counter:
+            #     if counter[num] >= 2:
+            #         res += 1
+            #         counter[num] -= 2
+            #         if counter[num] == 0:
+            #             del counter[num]
+            # elif k - num in counter and num in counter:
                 res += 1
                 counter[num] -= 1
                 if counter[num] == 0:
