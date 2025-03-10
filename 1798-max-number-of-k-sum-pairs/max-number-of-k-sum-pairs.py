447d4802315494f82aaae0621710c810303183da
Xujie Zheng <z374201743@gmail.com>
2025-03-10 15:47:38 -0400

    Time: 614 ms (5.03%) | Memory: 30.9 MB (9.16%) - LeetSync

--- 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.py
+++ 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.py
@@ -2,13 +2,28 @@ class Solution:
     def maxOperations(self, nums: List[int], k: int) -> int:
         counter = Counter(nums)
         res = 0
+        def update():
+            nonlocal res
+            res += 1
+            counter[num] -= 1
+            if counter[num] == 0:
+                del counter[num]
+            counter[k - num] -= 1
+            if counter[k - num] == 0:
+                del counter[k - num]
+    
         for num in nums:
             cond1 = k - num == num and num in counter and counter[num] >= 2
             cond2 = k - num in counter and num in counter
-            if (
-                k - num == num and num in counter and counter[num] >= 2 or
-                k - num != num and k - num in counter and num in counter
-                ):
+            if k - num == num and num in counter:
+                if counter[num] >= 2:
+                    update()
+            elif k - num in counter and num in counter:
+                update()
+            # if (
+            #      and counter[num] >= 2 or
+            #     k - num != num and k - num in counter and num in counter
+            #     ):
             # if k - num == num and num in counter:
             #     if counter[num] >= 2:
             #         res += 1
@@ -16,11 +31,4 @@ class Solution:
             #         if counter[num] == 0:
             #             del counter[num]
             # elif k - num in counter and num in counter:
-                res += 1
-                counter[num] -= 1
-                if counter[num] == 0:
-                    del counter[num]
-                counter[k - num] -= 1
-                if counter[k - num] == 0:
-                    del counter[k - num]
         return res
