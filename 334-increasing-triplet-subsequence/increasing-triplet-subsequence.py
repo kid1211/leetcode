9dbf88088089545030945847c3dd3a06b3928a6b
Xujie Zheng <z374201743@gmail.com>
2025-03-09 23:29:02 -0400

    Time: 27 ms (32.45%) | Memory: 37.3 MB (96.58%) - LeetSync

---
+++ 334-increasing-triplet-subsequence/increasing-triplet-subsequence.py
@@ -0,0 +1,15 @@
+class Solution:
+    def increasingTriplet(self, nums: List[int]) -> bool:
+        i = j = + sys.maxsize
+
+        for num in nums:
+            # print("before:", i, j, k, num)
+            if num <= i:
+                i = num
+            elif num <= j:
+                j = num
+
+            if num > j:
+                return True
+            # print("after:", i, j, k, num)
+        return False
