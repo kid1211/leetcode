f556eb68e4238262118874e810928c8399896af0
Xujie Zheng <z374201743@gmail.com>
2025-03-10 13:55:15 -0400

    Time: 3 ms (39.34%) | Memory: 18 MB (46.75%) - LeetSync

---
+++ 443-string-compression/string-compression.py
@@ -0,0 +1,35 @@
+class Solution:
+    def compress(self, chars: List[str]) -> int:
+        # edge
+        def writeAns(char, length):
+            nonlocal left
+            if not char:
+                return
+            if length == 1:
+                chars[left] = char
+                left += 1
+                return
+
+            for d in [char] + list(str(length)):
+                chars[left] = d
+                left += 1
+
+        lastChar = None
+        lastLength = 0
+        left = 0
+        chars += " "
+        for right in range(len(chars)):
+            if lastChar == chars[right]:
+                lastLength += 1
+                continue
+            writeAns(lastChar, lastLength)
+            lastChar = chars[right]
+            lastLength = 1
+
+        return left
+
+
+# left spot for writing,
+# right point ,gooing until see a different chrater
+
+# write to left, keep going.
