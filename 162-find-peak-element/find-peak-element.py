db11ae5b7bcaac9f4aa1ea8ea71284d1fe732402
Xujie Zheng <z374201743@gmail.com>
2025-03-23 14:22:42 -0400

    Time: 0 ms (100.00%) | Memory: 17.8 MB (62.04%) - LeetSync

--- 162-find-peak-element/find-peak-element.py
+++ 162-find-peak-element/find-peak-element.py
@@ -12,8 +12,8 @@ class Solution:
             mid = (left + right) // 2
 
             if (
-                mid == 0 and nums[mid] > nums[mid + 1] or
-                mid == n - 1 and nums[mid] > nums[mid - 1] or
+                # mid == 0 and nums[mid] > nums[mid + 1] or
+                # mid == n - 1 and nums[mid] > nums[mid - 1] or
                 nums[mid - 1] < nums[mid] > nums[mid + 1]
                 ):
                 return mid
