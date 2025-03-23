b97cedff3c1e8ab81aee24fdd6dd41529dff7303
Xujie Zheng <z374201743@gmail.com>
2025-03-22 22:12:49 -0400

    Time: 23 ms (34.85%) | Memory: 18.2 MB (53.03%) - LeetSync

--- 649-dota2-senate/dota2-senate.py
+++ 649-dota2-senate/dota2-senate.py
@@ -3,17 +3,14 @@ class Solution:
         voted = deque(list(senate))
         ready = deque()
 
-        i = 0
         while voted:
             curr = voted.popleft()
-            
+
             if not ready or ready[0] == curr:
                 ready += [curr]
             else:
                 voted.append(ready.popleft())
-            i += 1
 
-            # print(voted, ready)
             if len(ready) + len(voted) == 1:
                 break
         val = ready[0] if ready else voted[0]
