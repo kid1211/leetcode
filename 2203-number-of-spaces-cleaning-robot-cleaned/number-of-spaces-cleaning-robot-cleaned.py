87569150a435e99692e930f0eaeda2627b81171b
Xujie Zheng <z374201743@gmail.com>
2025-04-01 21:42:35 -0400

    Time: 0 ms (100.00%) | Memory: 20.9 MB (98.13%) - LeetSync

---
+++ 2203-number-of-spaces-cleaning-robot-cleaned/number-of-spaces-cleaning-robot-cleaned.py
@@ -0,0 +1,34 @@
+class Solution:
+    def numberOfCleanRooms(self, room: List[List[int]]) -> int:
+        DIRECTIONS = (0, 1, 0, -1, 0)
+        rows, cols = len(room), len(room[0])
+        cleaned = 0
+
+        row, col = 0, 0
+        direction = 0
+
+        # Clean until we revisit a space facing the same direction
+        while not room[row][col] >> (direction + 1) & 1:
+            # If the robot hasn't cleaned this space yet, increment cleaned
+            if room[row][col] == 0:
+                cleaned += 1
+
+            # Mark the space as visited facing this direction
+            room[row][col] |= 1 << (direction + 1)
+
+            # Clean the next space straight ahead if it's empty and in the room
+            next_row = row + DIRECTIONS[direction]
+            next_col = col + DIRECTIONS[direction + 1]
+            if (
+                0 <= next_row < rows
+                and 0 <= next_col < cols
+                and room[next_row][next_col] != 1
+            ):
+                row = next_row
+                col = next_col
+
+            # Otherwise turn right and clean the current space
+            else:
+                direction = (direction + 1) % 4
+
+        return cleaned
