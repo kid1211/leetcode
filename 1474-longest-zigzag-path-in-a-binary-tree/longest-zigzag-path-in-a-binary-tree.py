104f58d9af1fc4643edf5edee1649dfdb701ffd0
Xujie Zheng <z374201743@gmail.com>
2025-04-06 17:04:44 -0400

    Time: 53 ms (81.91%) | Memory: 33.3 MB (40.35%) - LeetSync

--- 1474-longest-zigzag-path-in-a-binary-tree/longest-zigzag-path-in-a-binary-tree.py
+++ 1474-longest-zigzag-path-in-a-binary-tree/longest-zigzag-path-in-a-binary-tree.py
@@ -7,6 +7,7 @@
 class Solution:
     def longestZigZag(self, root: Optional[TreeNode]) -> int:
         res = 0
+
         def dfs(node, isLeft, step):
             nonlocal res
             if not node:
@@ -15,31 +16,27 @@ class Solution:
 
             dfs(node.left if isLeft else node.right, not isLeft, step + 1)
             if isLeft:
-                # dfs(node.left, False, step + 1)
-                dfs(node.right, True, 1)
+                dfs(node.right, isLeft, 1)
             else:
-                dfs(node.left, False, 1)
-                # dfs(node.right, True, step + 1)
+                dfs(node.left, isLeft, 1)
 
         dfs(root, True, 0)
         return res
 
 # class Solution:
 #     def longestZigZag(self, root: Optional[TreeNode]) -> int:
-#         self.pathLength = 0
-        
-#         def dfs(node, goLeft, steps):
-#             if not node:
-#                 return
 
-#             self.pathLength = max(self.pathLength, steps)
-#             if goLeft:
-#                 dfs(node.left, False, steps + 1)
-#                 dfs(node.right, True, 1)
-#             else:
-#                 # dfs(node.left, False, 1)
-#                 dfs(node.right, True, steps + 1)
-        
-#         dfs(root, True, 0)
-#         dfs(root, False, 0)
-#         return self.pathLength
+#         def dfs(node, isLeft):
+#             if not node:
+#                 return 0
+
+#             return max(
+#                 1 + dfs(node.left if isLeft else node.right, not isLeft),
+#                 dfs(node.right if isLeft else node.left, isLeft)
+#             )
+
+
+#         return max(
+#             dfs(root, True),
+#             dfs(root, False)
+#         ) - 1
