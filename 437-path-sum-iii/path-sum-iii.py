3dd8ba69dc578b912d24280d7963af64fcea77a5
Xujie Zheng <z374201743@gmail.com>
2025-04-06 14:36:41 -0400

    Time: 8 ms (53.17%) | Memory: 18.3 MB (68.85%) - LeetSync

---
+++ 437-path-sum-iii/path-sum-iii.py
@@ -0,0 +1,36 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
+        res = 0
+
+        hashmap = defaultdict(int)
+        # hashmap[root.val - targetSum] += 1
+
+        def dfs(node, curr):
+            nonlocal res
+            if not node:
+                return
+
+            curr += node.val
+
+            # One special case
+            if curr == targetSum:
+                res += 1
+            
+            res += hashmap[curr - targetSum]
+
+
+            hashmap[curr] += 1
+            dfs(node.left, curr)
+            dfs(node.right, curr)
+            hashmap[curr] -= 1
+            return
+
+        dfs(root, 0)
+        return res
+
