4a9e11c9a95884e8de9b1b3669f982d947d1b5b7
Xujie Zheng <z374201743@gmail.com>
2025-04-27 16:48:41 -0400

    Time: 3 ms (4.89%) | Memory: 17.7 MB (68.43%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -1,4 +1,4 @@
-maps = {
+mapping = {
     "2": "abc",
     "3": "def",
     "4": "ghi",
@@ -8,20 +8,20 @@ maps = {
     "8": "tuv",
     "9": "wxyz"
 }
+
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
         if not digits:
             return []
-        def dfs(i):
-            if i == len(digits) - 1:
-                return list(maps[digits[i]])
+
+        def dfs(idx):
+            if idx == len(digits) - 1:
+                return list(mapping[digits[idx]])
 
             res = []
-            for l in maps[digits[i]]:
-                for tmp in dfs(i + 1):
-                    res += [
-                        l + tmp
-                    ]
+            for l in mapping[digits[idx]]:
+                for suffix in dfs(idx + 1):
+                    res += [l + suffix]
             return res
-        
+
         return dfs(0)
