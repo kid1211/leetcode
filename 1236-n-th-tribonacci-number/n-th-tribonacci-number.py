ad61eeb4cf8b269f9a4f99fa9bdc624290b654d9
Xujie Zheng <z374201743@gmail.com>
2025-06-01 13:41:43 -0400

    Time: 0 ms (100.00%) | Memory: 18 MB (8.66%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.py
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.py
@@ -1,8 +1,12 @@
 class Solution:
     @cache
     def tribonacci(self, n: int) -> int:
-        if n == 0:
-            return 0
-        if n == 1 or n == 2:
+        if n == 0 or n == 1:
+            return n
+        if n == 2:
             return 1
-        return self.tribonacci(n - 1) + self.tribonacci(n - 2) + self.tribonacci(n - 3)
+        # if n == 3:
+        #     return 0 + 1 + 1
+        # if n == 3:
+        #     return 1 + 2 + 3
+        return  self.tribonacci(n - 1) + self.tribonacci(n - 2) + self.tribonacci(n - 3)
