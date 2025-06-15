dcc95a25b2cf398ec9849a5050062486cf760a6a
Xujie Zheng <z374201743@gmail.com>
2025-06-15 14:08:09 -0400

    Time: 136 ms (50.62%) | Memory: 23.3 MB (94.83%) - LeetSync

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.py
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -1,20 +1,32 @@
 class Solution:
     def maxProfit(self, prices: List[int], fee: int) -> int:
-        @cache
-        def dfs(day, isHolding):
-            if day == len(prices):
-                return 0
+        n = len(prices)
+        hold, free = [0] * n, [0] * n
 
-            doNothing = dfs(day + 1, isHolding)
-            if isHolding:
-                return max(
-                    doNothing,
-                    dfs(day + 1, not isHolding) + prices[day] - fee,
-                )
-            else:
-                return max(
-                    doNothing,
-                    dfs(day + 1, not isHolding) - prices[day]
-                )
-        
-        return dfs(0, False)
+        hold[0] = -prices[0]
+
+        for i in range(1, n):
+            # hold
+            hold[i] = max(hold[i - 1], free[i - 1] - prices[i])
+            free[i] = max(free[i - 1], hold[i - 1] + prices[i] - fee)
+
+        return free[-1]
+
+        # @cache
+        # def dfs(day, isHolding):
+        #     if day == len(prices):
+        #         return 0
+
+        #     doNothing = dfs(day + 1, isHolding)
+        #     if isHolding:
+        #         return max(
+        #             doNothing,
+        #             dfs(day + 1, not isHolding) + prices[day] - fee,
+        #         )
+        #     else:
+        #         return max(
+        #             doNothing,
+        #             dfs(day + 1, not isHolding) - prices[day]
+        #         )
+
+        # return dfs(0, False)
