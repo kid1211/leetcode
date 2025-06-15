d3e63121e34eacabd12ef8ba9101cf623d9e8a73
Xujie Zheng <z374201743@gmail.com>
2025-06-15 16:12:51 -0400

    Time: 626 ms (35.07%) | Memory: 20.7 MB (68.86%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,33 +1,27 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        
-        def twoSumSorted(left, right, target) -> [[int, int, int]]:
-            res = []
+        nums.sort()
+        print(nums)
+        n = len(nums)
+        res = []
+        for i in range(n):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            target = -nums[i]
+            left = i + 1
+            right = n - 1
             while left < right:
-                if res and nums[left] == nums[left - 1]:
-                    left += 1
-                    continue
-                if res and nums[right] == nums[right + 1]:
-                    right -= 1
-                    continue
+                if nums[left] + nums[right] == target:
+                    res += [[nums[i], nums[left], nums[right]]]
 
-                tmp = nums[left] + nums[right] + target
-
-                if tmp == 0:
-                    res += [[nums[left], nums[right], target]]
+                    while left < right and nums[left] == nums[left + 1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right - 1]:
+                        right -= 1
                     left += 1
                     right -= 1
-                elif tmp < 0:
+                elif nums[left] + nums[right] < target:
                     left += 1
                 else:
                     right -= 1
-            return res
-        
-        nums.sort()
-        n = len(nums)
-        res = []
-        for i in range(len(nums)):
-            if res and nums[i] == nums[i - 1]:
-                continue
-            res += twoSumSorted(i + 1, n - 1, nums[i])
         return res
