bedce05570d18c67c554d0900edb69dc34ebaa28
Xujie Zheng <z374201743@gmail.com>
2025-06-22 13:52:15 -0400

    Time: 41 ms (26.84%) | Memory: 20.3 MB (93.37%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -1,10 +1,48 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        h = defaultdict(int)
-        h[0] = 1
-        res = rolling = 0
+        # cannot use set, [1,-1,0]
+        maps = defaultdict(int)
+        maps[0] = 1
+        rolling = 0
+        res = 0
         for num in nums:
             rolling += num
-            res += h[rolling - k]
-            h[rolling] += 1
+            if rolling - k in maps:
+                res += maps[rolling - k]
+            maps[rolling] += 1
+            # print(rolling, maps)
         return res
+
+        # n = len(nums)
+        # res = right = cumulate = 0
+
+        # for left in range(n):
+        #     while right < n and cumulate < k:
+        #         cumulate += nums[right]
+        #         right += 1
+
+        #     if right > left and cumulate == k:
+        #         res += 1
+
+        #     cumulate -= nums[left]
+        # return res
+
+
+        # def dfs(startIdx, target):
+        #     if startIdx >= len(nums):
+        #         return 0
+
+        #     res = 0
+        #     for i in range(startIdx, len(nums)):
+        #         nextTarget = target - nums[i]
+        #         if nextTarget < 0:
+        #             continue # BREAK?
+        #         # if nextTarget == 0:
+        #         #     res += 1
+        #         res += dfs(i + 1, nextTarget)
+
+        #     if target == 0:
+        #         res += 1
+        #     return res
+
+        # return dfs(0, k)
