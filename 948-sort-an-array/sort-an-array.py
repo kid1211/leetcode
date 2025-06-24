12b8382ce9b07b9cdf3276d24e28585cf76eeea8
Xujie Zheng <z374201743@gmail.com>
2025-06-23 22:50:10 -0400

    Time: 882 ms (12.34%) | Memory: 25.6 MB (36.01%) - LeetSync

--- 948-sort-an-array/sort-an-array.py
+++ 948-sort-an-array/sort-an-array.py
@@ -1,46 +1,25 @@
 class Solution:
-    # Radix sort function.
-    def radix_sort(self, nums: List[int]) -> List[int]:
-        # Find the absolute maximum element to find max number of digits.
-        max_element = nums[0]
-        for val in nums:
-            max_element = max(abs(val), max_element)
-
-        max_digits = 0
-        while max_element > 0:
-            max_digits += 1
-            max_element = max_element // 10
-
-        place_value = 1
+    def sortArray(self, nums: List[int]) -> List[int]:
+        if len(nums) <= 1:
+            return nums
         
-        # Bucket sort function for each place value digit.
-        def bucket_sort():
-            buckets = [[] for i in range(10)]
-            # Store the respective number based on it's digit.
-            for val in nums:
-                digit = abs(val) / place_value
-                digit = int(digit % 10)
-                buckets[digit].append(val)
+        mid = (len(nums)) // 2
+        leftSorted = self.sortArray(nums[:mid])
+        rightSorted = self.sortArray(nums[mid:])
 
-            # Overwrite 'nums' in sorted order of current place digits.
-            index = 0
-            for digit in range(10):
-                for val in buckets[digit]:
-                    nums[index] = val
-                    index += 1
+        res = []
+        left = right = 0
+        leftN, rightN = len(leftSorted), len(rightSorted)
 
-        # Radix sort, least significant digit place to most significant.      
-        for _ in range(max_digits):
-            bucket_sort()
-            place_value *= 10
+        while left < leftN or right < rightN:
+            leftVal = leftSorted[left] if left < leftN else sys.maxsize
+            rightVal = rightSorted[right] if right < rightN else sys.maxsize
 
-        # Seperate out negatives and reverse them. 
-        positives = [val for val in nums if val >= 0]
-        negatives = [val for val in nums if val < 0]
-        negatives.reverse()
-
-        # Final 'arr' will be 'negative' elements, then 'positive' elements.
-        return negatives + positives
-            
-    def sortArray(self, nums: List[int]) -> List[int]:  
-        return self.radix_sort(nums)                                                      
+            if leftVal < rightVal:
+                res += [leftSorted[left]]
+                left += 1
+            else:
+                res += [rightSorted[right]]
+                right += 1
+        
+        return res
