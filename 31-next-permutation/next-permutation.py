e70fa11c97886cfa2b3690f83b3976d5ce0fe3cb
Xujie Zheng <z374201743@gmail.com>
2025-06-28 14:51:32 -0400

    Time: 0 ms (100.00%) | Memory: 17.7 MB (56.73%) - LeetSync

--- 31-next-permutation/next-permutation.py
+++ 31-next-permutation/next-permutation.py
@@ -2,36 +2,50 @@ class Solution:
     def nextPermutation(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
+
+        1. find pivot, from right to left increasing
+        2. swap pivot find the largest number that is larger than the pivot
+        3. reverse the res
+
+        1 8 3 2 6 5
         """
-        # 183265 -> 183526
-        # 右边往左边升序结束的地方, 的前一位和最生序的开头swap,然后把后面reverse
-        # 2, 5 swap
-        # 183625
-
-        # 87654321 => -1, 没得换, 直接reverse
-        # 18765432 => 2, 1 swap 28765431, -> reversed 8765431
-
-        # 3987421 => 4 和3换
-        # 4987321 ==> reverse 4123789
-        if len(nums) == 1:
-            return
-
         n = len(nums)
+
+        if n == 1:
+            return
+        # 1
         i = n - 2
         while i >= 0 and nums[i] >= nums[i + 1]:
             i -= 1
 
-        if i >= 0:
-            for j in range(n - 1, -1, -1):
-                if nums[j] > nums[i]:
-                    break
+        # 2
+        for j in range(n - 1, -1, -1):
+            if nums[j] > nums[i]:
+                break
+
+        # print(i, j, n, nums[i], nums[j])
+        if i != -1:
             nums[i], nums[j] = nums[j], nums[i]
-        print(nums)
-        i, j = i + 1 if i >= 0 else 0, n - 1
-        while i < j:
-            nums[i], nums[j] = nums[j], nums[i]
-            i += 1
-            j -= 1
+
+        # 3
+        left, right = i + 1, n - 1
+        while left < right:
+            nums[left], nums[right] = nums[right], nums[left]
+            left += 1
+            right -= 1
+        return nums
+
+        # if i >= 0:
 
 
+"""
+1, 2, 3, 4, 5
+2, 1, 3, 4, 5
+
+
+1, 2, 5, 3, 4
+1, 2, 5, 4, 3
+1, 5, 2, 3, 4
+1, 5, 2, 4, 3
+"""
         
