86dd109a86ada0f62d97e3ba4bb136e0b20e0357
Xujie Zheng <z374201743@gmail.com>
2025-07-19 15:13:50 -0400

    Time: 18 ms (37.78%) | Memory: 18.4 MB (35.18%) - LeetSync

--- 51-n-queens/n-queens.py
+++ 51-n-queens/n-queens.py
@@ -1,61 +1,47 @@
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
-        
         res = []
-        col_set = set()
-        plus_set = set()
-        minus_set = set()
+        xSet = set()
+        ySet = set()
+        xPySet = set()
+        xMySet = set()
 
-        def dfs(row, curr):
+        def dfs(curr):
             nonlocal res
             if len(curr) == n:
                 res += [curr]
                 return
-            
+
+            row = len(curr)
             for col in range(n):
-                if col in col_set:
+                if row in xSet:
                     continue
-                
-                if row + col in plus_set:
+                if col in ySet:
                     continue
-                
-                if row - col in minus_set:
+                if row + col in xPySet:
                     continue
-                
-                col_set.add(col)
-                plus_set.add(row + col)
-                minus_set.add(row - col)
+                if row - col in xMySet:
+                    continue
 
-                dfs(row + 1, curr + [col])
+                xSet.add(row)
+                ySet.add(col)
+                xPySet.add(row + col)
+                xMySet.add(row - col)
+                dfs(curr + [col])
+                xSet.remove(row)
+                ySet.remove(col)
+                xPySet.remove(row + col)
+                xMySet.remove(row - col)
 
-                col_set.remove(col)
-                plus_set.remove(row + col)
-                minus_set.remove(row - col)
-                pass
-        
-        dfs(0, [])
-        
-        # draw board
-        converted = []
-        for ans in res:
-            sol = []
-            for pos in ans:
-                tmp = ["." for _ in range(n)]
-                tmp[pos] = "Q"
-                sol += ["".join(tmp)]
-                # sol += 
-            converted += [sol]
+        def drawBoard(colList):
+            res = []
+            for col in colList:
+                res += ["". join(col * '.' + 'Q' + (n - col - 1) * '.')]
+            return res
 
+        dfs([])
+        print(res)
 
-        return converted
-    
-
-# draw board
-    # col number to => ["..Q.",".Q.." ] 
-    # [2, 1, 3, 0] For example
-
-# how to generate that?
-#  go thorugh each column at a time
-# make sure no vertical or diagonal (x + y, and x-y), no need for horizontal, because one at a time
-# put it in, continue, then pop it out and continue
-# no need for memo, so use index, and curr
+        return [
+            drawBoard(colList) for colList in res
+        ]
