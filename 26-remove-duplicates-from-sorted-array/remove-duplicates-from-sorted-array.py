a7961d6cf0c9c2a616300ecd88fc8b1adadee12a
Xujie Zheng <z374201743@gmail.com>
2025-07-26 16:22:48 -0400

    Time: 0 ms (100.00%) | Memory: 18.9 MB (74.53%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -1,14 +1,28 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
+        if len(nums) < 2:
+            return len(nums)
         n = len(nums)
+        l = 1
+        for r in range(1, n):
+            if nums[r] == nums[l - 1]:
+                continue
+            nums[l] = nums[r]
+            l += 1
+            # if l == 0 or nums[l]
+        return l
 
-        j = 0
-        last_update = 0
-        for i in range(n):
-            while j < n and nums[i - 1] == nums[j]:
-                j += 1
-            if j < n:
-                nums[i], nums[j] = nums[j], nums[i]
-                j += 1
-                last_update = max(last_update, i)
-        return last_update + 1
+# class Solution:
+#     def removeDuplicates(self, nums: List[int]) -> int:
+#         n = len(nums)
+
+#         j = 0
+#         last_update = 0
+#         for i in range(n):
+#             while j < n and nums[i - 1] == nums[j]:
+#                 j += 1
+#             if j < n:
+#                 nums[i], nums[j] = nums[j], nums[i]
+#                 j += 1
+#                 last_update = max(last_update, i)
+#         return last_update + 1
