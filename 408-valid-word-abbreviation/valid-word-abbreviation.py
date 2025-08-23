40c69a3a96d36eaf33148f704de138827cb35647
Xujie Zheng <z374201743@gmail.com>
2025-08-23 13:59:31 -0400

    Time: 0 ms (100.00%) | Memory: 17.9 MB (38.10%) - LeetSync

---
+++ 408-valid-word-abbreviation/valid-word-abbreviation.py
@@ -0,0 +1,29 @@
+class Solution:
+    def validWordAbbreviation(self, word: str, abbr: str) -> bool:
+        number = []
+        word = word + "a"
+        abbr = abbr + "a"
+        
+        j = 0 # word
+        for i in range(len(abbr)):
+            c = abbr[i]
+            if c.isnumeric():
+                number += [c]
+                continue
+
+            if not number and (j >= len(word) or abbr[i] != word[j]):
+                return False
+            elif len(number) > 0 and  number[0] == "0":
+                return False
+    
+            tmp = int("".join(number)) if number else 0
+            # print('number', number)
+            j += tmp
+            # print(tmp, (i, abbr[i]), (j, word[j]))
+            if j >= len(word) or abbr[i] != word[j]:
+                return False
+            j += 1
+            number = []
+        # print(j, len(word), number)
+        # tmp = int("".join(number)) if number else 0
+        return len(word) == j
