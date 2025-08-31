b01b3672cd307797236dcf1cf5375949383bd644
Xujie Zheng <z374201743@gmail.com>
2025-08-31 13:20:34 -0400

    Time: 51 ms (71.92%) | Memory: 20.8 MB (96.45%) - LeetSync

--- 1790-lowest-common-ancestor-of-a-binary-tree-iii/lowest-common-ancestor-of-a-binary-tree-iii.py
+++ 1790-lowest-common-ancestor-of-a-binary-tree-iii/lowest-common-ancestor-of-a-binary-tree-iii.py
@@ -11,7 +11,9 @@ class Node:
 class Solution:
     def lowestCommonAncestor(self, p: 'Node', q: 'Node') -> 'Node':
         a, b = p, q
+
         while a != b:
-            a = a.parent if a.parent else q
-            b = b.parent if b.parent else p
+            a = a.parent if a else q
+            b = b.parent if b else p
+        
         return a
