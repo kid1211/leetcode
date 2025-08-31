d859481b50234ff116eabc1778ea50c30f451e04
Xujie Zheng <z374201743@gmail.com>
2025-08-31 16:46:48 -0400

    Time: 75 ms (54.64%) | Memory: 22.8 MB (93.26%) - LeetSync

--- 912-random-pick-with-weight/random-pick-with-weight.py
+++ 912-random-pick-with-weight/random-pick-with-weight.py
@@ -1,30 +1,54 @@
 class Solution:
 
     def __init__(self, w: List[int]):
-        rolling = 0
-        self.l = []
-        for i in w:
-            rolling += i
-            self.l += [rolling]
-        print(self.l)
+        for i in range(1, len(w)):
+            w[i] += w[i - 1]
+        self.w = w
+        print(self.w)
 
     def pickIndex(self) -> int:
-        target = random.randint(0, self.l[-1])
-        left, right = 0, len(self.l) - 1
-
+        if not self.w:
+            return -1
+        target = uniform(0, self.w[-1]) # maybe +1
+        
+        left, right = 0, len(self.w) - 1
+        
         while left + 1 < right:
             mid = (left + right) // 2
-
-            if self.l[mid] <= target:
+            if self.w[mid] < target:
                 left = mid
             else:
                 right = mid
-
-        return left if self.l[left] > target else right
+        
+        if self.w[left] >= target:
+            return left
+        else:
+            return right
+        
+    # def pickIndex(self) -> int:
+    #     target = random.uniform(0, self.w[-1])
+    #     start, end = 0, len(self.w) - 1
+        
+    #     while start + 1 < end:
+    #         mid = (start + end) // 2
+            
+    #         if self.w[mid] >= target:
+    #             end = mid
+    #         else:
+    #             start = mid
+        
+    #     return start if self.w[start] >= target else end
+                
 
 
 # Your Solution object will be instantiated and called as such:
 # obj = Solution(w)
 # param_1 = obj.pickIndex()
-[1, 3, 2]
-[1, 4, 6] # 0 - 4 randomly pick, 0-> 0, 1, 5, -> 2
+
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(w)
+# param_1 = obj.pickIndex()
+
+# [1 , 4, 10, 2, 3]
+# [1, 5, 15, 17,20] # rand(0, 20) => 6
