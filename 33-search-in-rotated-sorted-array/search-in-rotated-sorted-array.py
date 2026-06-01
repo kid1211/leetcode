1848bec4c6574d8d293ea7d968cf462df43a16df
Xujie Zheng <z374201743@gmail.com>
2026-05-31 21:38:24 -0400

    Time: 0 ms (100.00%) | Memory: 19.3 MB (77.90%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -1,24 +1,32 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        left, right = 0, len(nums) - 1
+        n = len(nums)
+        left, right = 0, n - 1
 
-        while left + 1 < right:
+        # Find the index of the pivot element (the smallest element)
+        while left <= right:
             mid = (left + right) // 2
-
-            if target == nums[mid]:
-                return mid
-
-            if nums[left] <= target < nums[mid]:
-                right = mid
-            elif nums[mid] < target <= nums[right]:
-                left = mid
-            elif nums[left] < nums[mid]:
-                left = mid
+            if nums[mid] > nums[-1]:
+                left = mid + 1
             else:
-                right = mid
-        
-        if nums[left] == target:
-            return left
-        if nums[right] == target:
-            return right
-        return -1
+                right = mid - 1
+
+        # Binary search over an inclusive range [left_boundary ~ right_boundary]
+        def binarySearch(left_boundary, right_boundary, target):
+            left, right = left_boundary, right_boundary
+            while left <= right:
+                mid = (left + right) // 2
+                if nums[mid] == target:
+                    return mid
+                elif nums[mid] > target:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+            return -1
+
+        # Binary search over elements on the pivot element's left
+        if (answer := binarySearch(0, left - 1, target)) != -1:
+            return answer
+
+        # Binary search over elements on the pivot element's right
+        return binarySearch(left, n - 1, target)
